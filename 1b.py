f0abe16549f2462e035e814bf9a46316d52ee7d6
Madu ✨ <maduvime@eq.ufrj.br>
2024-12-28 21:03:16 -0300

    Exercício Semana 1 - Letra B

---
+++ 1b.py
@@ -0,0 +1,15 @@
+import math
+
+n = 2
+somatorio = 0
+
+while n < 100000:
+    somatorio += 1/(n*math.log(n, math.e)**2)
+    print(somatorio)
+    n = n + 1
+
+print("\n >>> ACABOU O LAÇO!",'\n')
+
+# Acredito que esteja CONVERGINDO para 2.0
+
+# Mesmo com WHILE TRUE há adições infinitas muito pequenas ao valor de 2.0
