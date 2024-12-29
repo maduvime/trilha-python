92d0e4ee108af07eb1fe67632c7b2e9dc1722c76
Madu ✨ <maduvime@eq.ufrj.br>
2024-12-28 21:48:37 -0300

    Exercício Semana 1 - Letra B

--- 1b.py
+++ 1b.py
@@ -13,3 +13,5 @@ print("\n >>> ACABOU O LAÇO!",'\n')
 # Acredito que esteja CONVERGINDO para 2.0
 
 # Mesmo com WHILE TRUE há adições infinitas muito pequenas ao valor de 2.0
+
+# Porém, ainda que as adições sejam muito pequenas, continua havendo adições, por isso não estou 100% convicta de que seja CONVERGENTE
