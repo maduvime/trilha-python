a9f8bc4f1554f7810cb1f05eb214e353f5abd8f4
Madu ✨ <maduvime@eq.ufrj.br>
2024-12-29 19:05:37 -0300

    Introdução a Funções

--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,13 @@
 # O que são funções?
 # definir um comando que salva um conjunto de informações
 
-#def comando(x, y):  # "def" é usada para criar o comando, e em '()' ficam os parâmetros, se forem necessários
-   # soma = x + y    # código a ser executado pelo comando
-   # print(soma) # -> se usar print, o comando vai apenas ser printado em vez de executado
+def comando(x, y):  # "def" é usada para criar o comando, e em '()' ficam os parâmetros, se forem necessários
+    soma = x + y    # código a ser executado pelo comando
+    print(soma) # -> se usar print, o comando vai apenas ser printado em vez de executado
     #return soma     # o certo é usar "RETURN"
 
-#comando(x=10, y=20) # se usar PRINT aqui de novo, será printado 'None'
+comando(x=10, y=20) # se usar PRINT aqui de novo, será printado 'None'
+
 
 def comando(x, y):
     soma = x + y
